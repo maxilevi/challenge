c592b867c96666f8d74ec0ef351b39ebf278578d
Maximiliano Levi <maxilevi77@gmail.com>
2022-02-21 22:36:28 -0300

    add deposit tests

--- tests/deposit_test.py
+++ tests/deposit_test.py
@@ -7,7 +7,19 @@ def pool():
 
 def test_depositing_to_pool(pool):
     balance = accounts[1].balance()
-    pool.deposit()
-    accounts[1].transfer(accounts[1], "10 ether", gas_price=0)
+    pool.deposit({'from': accounts[1], 'value': "0.1 ether"})
 
-    assert balance - "10 ether" == accounts[0].balance()
+    assert balance - "0.1 ether" == accounts[1].balance()
+    assert pool.totalDeposits() == "0.1 ether"
+
+
+def test_multiple_deposits_add_up(pool):
+    balanceA = accounts[1].balance()
+    balanceB = accounts[2].balance()
+    pool.deposit({'from': accounts[1], 'value': "0.5 ether"})
+    pool.deposit({'from': accounts[2], 'value': "0.1 ether"})
+    pool.deposit({'from': accounts[2], 'value': "0.1 ether"})
+
+    assert balanceA - "0.5 ether" == accounts[1].balance()
+    assert balanceB - "0.2 ether" == accounts[2].balance()
+    assert pool.totalDeposits() == "0.7 ether"
